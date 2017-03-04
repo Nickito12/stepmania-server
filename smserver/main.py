41cf8b607b37de1159f4102b4765c74e7267cc00
Nickito12 <abramlujan@gmail.com>
2017-03-04 18:23:58 -0300

    Basic main to start the server

--- smserver/main.py
+++ smserver/main.py
@@ -1,1 +1,30 @@
 
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+import sys
+import signal
+
+from smserver import conf
+from smserver import server
+from smserver import start_up
+
+def main():
+    start_up.start_up(*sys.argv[1:])
+
+    serv = server.StepmaniaServer()
+
+    try:
+        signal.signal(signal.SIGTERM, lambda *_: sys.exit())
+        signal.signal(signal.SIGHUP, lambda *_: serv.reload())
+    except AttributeError:
+        pass
+
+    try:
+        serv.start()
+    except (KeyboardInterrupt, SystemExit):
+        serv.stop()
+        sys.exit()
+
+if __name__ == "__main__":
+    main()
